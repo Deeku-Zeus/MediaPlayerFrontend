b4020edc34e7ff10fc46e18b156ba70fd1a55264
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-28 18:09:06 +0530

    asset path changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-	import SteamingSite from '../components/SteamingSite.svelte';
+	import SteamingSite from '@components/SteamingSite.svelte';
 	//import VideoPlayer2 from '../components/VideoPlayer2.svelte';
 	//import EcomSite from '../components/EcomSite.svelte';
 </script>
 
-<link rel="stylesheet" href="@static/fonts/css/all.min.css" />
+<link rel="stylesheet" href="../../static/fonts/css/all.min.css" />
 <main class="p-8">
 	<h1 class="text-3xl font-bold mb-6 text-yellow-500">Ecom Video Player</h1>
 	<SteamingSite />
